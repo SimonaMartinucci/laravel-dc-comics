d694b83058f6f1e34da41f76e0cd01d6404eef10
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-09-18 15:10:04 +0200

    feat: add store validation

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -30,6 +30,37 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required|min:3|max:100',
+            'description' => 'required|min:3|max:255',
+            'thumb' => 'required|min:3|max:250',
+            'price' => 'required|numeric',
+            'series' => 'required|min:3|max:100',
+            'sale_date' => 'required|date',
+            'type' => 'required|min:3|max:100'
+        ],
+        [
+            'name.required' => 'Il nome è un campo obbligatorio',
+            'name.min' => 'Il nome deve avere almeno :min caratteri',
+            'name.max' => 'Il nome deve avere massimo :max caratteri',
+            'description.required' => 'La descrizione è un campo obbligatorio',
+            'description.min' => 'La descrizione deve avere almeno :min caratteri',
+            'description.max' => 'La descrizione deve avere massimo :max caratteri',
+            'thumb.required' => 'L\'immagine è un campo obbligatorio',
+            'thumb.min' => 'L\'immagine deve avere almeno :min caratteri',
+            'thumb.max' => 'L\'immagine deve avere massimo :max caratteri',
+            'price.required' => 'Il prezzo è un campo obbligatorio',
+            'price.numeric' => 'Il prezzo deve essere un numero',
+            'series.required' => 'La serie è un campo obbligatorio',
+            'series.min' => 'La serie deve avere almeno :min caratteri',
+            'series.max' => 'La serie deve avere massimo :max caratteri',
+            'sale_date.required' => 'La data di vendita è un campo obbligatorio',
+            'sale_date.date' => 'La data di vendita di essere una data',
+            'type.required' => 'Il tipo è un campo obbligatorio',
+            'type.min' => 'Il tipo deve avere almeno :min caratteri',
+            'type.max' => 'Il tipo deve avere massimo :max caratteri',
+        ]);
+
         $data = $request->all();
 
         $newComic = new Comic();
